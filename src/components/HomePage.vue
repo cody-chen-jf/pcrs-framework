3a089caa84f7771ad4302dad5957e1c136fd58ea
cody-chen-jf <459741515@qq.com>
2018-05-26 20:56:24 +0800

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home-page">
+    <div class="home-page-title">This is Home Page</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      testData: ['a', 'b', 'c']
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  @import './../common/css/base.scss';
+
+  .home-page-title {
+    color: $base-color;
+  }
+
+  /*.test_img {*/
+    /*background-image: url('./../assets/logo.png');*/
+    /*width: 100px;*/
+    /*height: 200px;*/
+  /*}*/
+</style>
